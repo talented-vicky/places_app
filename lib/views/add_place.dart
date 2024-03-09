cdd394ca41e285b4758e17665e9ea86ed67fef96
talented_vicky <victorotubure7@gmail.com>
2024-03-09 22:07:24 +0100

    stored uploaded image in docs dir

--- lib/views/add_place.dart
+++ lib/views/add_place.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 class AddPlace extends StatefulWidget {
   static const routeName = '/add-place';
@@ -14,6 +17,20 @@ class AddPlace extends StatefulWidget {
 class _AddPlaceState extends State<AddPlace> {
   final _txtCtrl = TextEditingController();
   File? imagefile;
+
+  Future<void> _pickImage() async {
+    final imgPicker = ImagePicker();
+    final img =
+        await imgPicker.pickImage(source: ImageSource.gallery, maxWidth: 500);
+    // set permissions at ios>>runner>>base.lproj>>info.plist
+    setState(() async {
+      imagefile = File(img!.path);
+
+      final appDir = await getApplicationDocumentsDirectory();
+      final name = basename(imagefile!.path);
+      await imagefile!.copy('${appDir.path}/$name');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +69,7 @@ class _AddPlaceState extends State<AddPlace> {
                       ),
                       const SizedBox(width: 50),
                       TextButton.icon(
-                          onPressed: () {},
+                          onPressed: () => _pickImage(),
                           icon: const Icon(Icons.camera),
                           label: const Text('Take Picture'))
                     ],
@@ -70,3 +87,23 @@ class _AddPlaceState extends State<AddPlace> {
     );
   }
 }
+
+// class MyCamDelegate extends ImagePickerCameraDelegate {
+//   Future<void> _pickImage(stuff) async {
+//     final imgPicker = ImagePicker();
+//     final img =
+//         await imgPicker.pickImage(source: ImageSource.camera, maxWidth: 500);
+//     // set permissions at ios>>runner>>base.lproj>>info.plist
+//   }
+
+//   @override
+//   Future<void> takePics(
+//       {ImagePickerCameraDelegateOptions options =
+//           const ImagePickerCameraDelegateOptions()}) async {
+//     return _pickImage(options.preferredCameraDevice);
+//   }
+// }
+
+// void setupCamDelegate() {
+//   final ImagePickerPlatform inst = ImagePickerPlatform()
+// }
